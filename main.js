a0346370d20020e2f7e609bb6c2baca4ccb64a55
Kam Beh <kam.w.beh@gmail.com>
2015-09-13 23:14:44 +1000

    Minor changes to code
    Re-factor code to reduce repetitive code in listener block.

--- main.js
+++ main.js
@@ -14,46 +14,45 @@ var ractive = new Ractive({
   }
 });
 
-var listener = ractive.on({
-  neg25: function () {
-	var value = ractive.get( 'tbar');
-	var result = parseFloat(value - 0.25);
-	    console.log( 'neg 25! ' + result);
-		
+var setNormalBar = function(result) {
 	if(result >= 0){
 		ractive.set( 'tbar', result);
 		ractive.set( 'barcolor', ractive.get('normalcolor'));
-	}	
-  },
-  pos25: function () {
-	var value = ractive.get( 'tbar');
-	var result = parseFloat(value + 0.25);
-	    console.log( 'pos 25! ' + result);
+	}		
+};
+
+var setExceededBar = function(result) {
 	if(result <= 1){
 		ractive.set( 'tbar', result);
 	}
 	else{
 		ractive.set( 'barcolor', ractive.get('exceedcolor'));
 	}	
+};
+
+var listener = ractive.on({
+  neg25: function () {
+	var value = ractive.get( 'tbar');
+	var result = parseFloat((value - 0.25).toFixed(2));
+	    console.log( 'neg 25! ' + result);
+	setNormalBar(result);
+  },
+  pos25: function () {
+	var value = ractive.get( 'tbar');
+	var result = parseFloat((value + 0.25).toFixed(2));
+	    console.log( 'pos 25! ' + result);
+	setExceededBar(result);
   },
   neg10: function () {
 	var value = ractive.get( 'tbar');
 	var result = parseFloat((value - 0.1).toFixed(2));
 		console.log( 'neg 10! ' + result);
-	if(result >= 0){
-		ractive.set( 'tbar', result);
-		ractive.set( 'barcolor', ractive.get('normalcolor'));
-	}
+	setNormalBar(result);
   },
   pos10: function () {
 	var value = ractive.get( 'tbar');
 	var result = parseFloat((value + 0.1).toFixed(2));
 		console.log( 'pos 10! ' + result);
-	if(result <= 1){
-		ractive.set( 'tbar', result);
-	}
-	else{
-		ractive.set( 'barcolor', ractive.get('exceedcolor'));
-	}
-  }
+	setExceededBar(result);
+  },
 });
